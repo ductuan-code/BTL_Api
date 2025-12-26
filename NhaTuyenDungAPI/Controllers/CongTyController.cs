1686e23c2fc8b8b1b5d7536c23b017a72051be6d
NguyenQuocViet26122005 <vviet2612@gmail.com>
2025-12-26 14:44:53 +0700

    vietcongty

---
+++ NhaTuyenDungAPI/Controllers/CongTyController.cs
@@ -0,0 +1,31 @@
+﻿using BLL.CongTy;
+using DTO.CongTy;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NhaTuyenDungAPI.Controllers
+{
+    [ApiController]
+    [Route("api/employer/companies")]
+    public class CongTyController : ControllerBase
+    {
+        private readonly ICongTyService _service;
+
+        public CongTyController(ICongTyService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{maNguoiDung}")]
+        public IActionResult GetMyCompanies(Guid maNguoiDung)
+        {
+            return Ok(_service.GetMyCompanies(maNguoiDung));
+        }
+
+        [HttpPost("{maNguoiDung}")]
+        public IActionResult Create(Guid maNguoiDung, TaoCongTyDto dto)
+        {
+            var result = _service.TaoCongTy(dto, maNguoiDung);
+            return result ? Ok("Tạo công ty thành công") : BadRequest("Tạo công ty thất bại");
+        }
+    }
+}
--- NhaTuyenDungAPI/Program.cs
+++ NhaTuyenDungAPI/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<DAL.CongTy.ICongTyRepository, DAL.CongTy.CongTyRepository>();
+builder.Services.AddScoped<BLL.CongTy.ICongTyService, BLL.CongTy.CongTyService>();
 
 
 var app = builder.Build();
