65a526338ae9ccef859a3f5dcc9a5167ed2f58ca
NguyenQuocViet26122005 <vviet2612@gmail.com>
2025-12-26 14:38:04 +0700

    vietcongty

---
+++ NhaTuyenDungAPI/DAL/CongTy/ICongTyRepository.cs
@@ -0,0 +1,10 @@
+﻿using DTO.CongTy;
+
+namespace DAL.CongTy
+{
+    public interface ICongTyRepository
+    {
+        List<CongTyDto> GetByEmployer(Guid maNguoiDung);
+        bool Create(TaoCongTyDto dto, Guid maNguoiDung);
+    }
+}
---
+++ NhaTuyenDungAPI/DTO/CongTy/CongTyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO.CongTy
+{
+    public class CongTyDto
+    {
+        public Guid MaCongTy { get; set; }
+        public string TenCongTy { get; set; }
+        public string Website { get; set; }
+        public string MoTa { get; set; }
+    }
+}
