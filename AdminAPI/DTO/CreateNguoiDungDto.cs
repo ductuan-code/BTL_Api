5af4c85ea1fed0332e9a2ef3b0dc172513491593
ductuan-code <nguyenductuan8910@gmail.com>
2025-12-26 15:54:43 +0700

    viet noi it thoi em

---
+++ AdminAPI/DTO/CreateNguoiDungDto.cs
@@ -0,0 +1,9 @@
+﻿public class CreateNguoiDungDto
+{
+    public string Email { get; set; }
+    public string MatKhauHash { get; set; }
+    public string? HoTen { get; set; }
+    public string? SoDienThoai { get; set; }
+    public int MaQuyen { get; set; }
+}
+
