8221d518545b0a774a59ac5956ef96b0b902e6e5
ductuan-code <nguyenductuan8910@gmail.com>
2026-01-04 23:56:25 +0700

    viec lam

---
+++ AdminAPI/Controllers/ViecLamController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/vieclam")]
+public class ViecLamController : ControllerBase
+{
+    private readonly ViecLamBLL _bll;
+
+    public ViecLamController(ViecLamBLL bll)
+    {
+        _bll = bll;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_bll.GetAll());
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateViecLamDto dto)
+    {
+        _bll.Create(dto);
+        return Ok("Tạo việc làm thành công");
+    }
+}
+
---
+++ AdminAPI/DTO/UpdateViecLamDto.cs
@@ -0,0 +1,13 @@
+﻿public class UpdateViecLamDto
+{
+    public string TieuDe { get; set; }
+    public string? DiaDiem { get; set; }
+    public string? LoaiHinhCongViec { get; set; }
+    public string? MoTa { get; set; }
+    public string? YeuCau { get; set; }
+    public string? TrachNhiem { get; set; }
+    public int? LuongToiThieu { get; set; }
+    public int? LuongToiDa { get; set; }
+    public DateTime? NgayHetHan { get; set; }
+    public bool DuocHienThi { get; set; }
+}
